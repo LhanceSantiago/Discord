5cc6b8dfe47b89082ebc2bbea8731785beb6ff15
lans <139159115+772nacho@users.noreply.github.com>
2024-12-06 23:52:38 +0800

    dropdown 100

--- dist/js/questDropdown.js
+++ dist/js/questDropdown.js
@@ -1,5 +1,16 @@
 const dropdown = document.querySelector('.questDropdown');
 const dropdownLinks = document.querySelector('.quests__dropdown');
+const subnav = document.querySelector('.subnav');
+
 dropdown.addEventListener("click", () => {
     dropdownLinks.classList.toggle("open");
 });
+
+window.addEventListener("scroll", () =>   {
+    console.log(scrollY);
+    if ((scrollY) >= 1 ) {
+     subnav.classList.add("fixed");
+    } else {
+     subnav.classList.remove("fixed");
+    }
+  });
