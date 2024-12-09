1d6d9e4691511a221a9339151a18da9c3fbc70e4
lans <139159115+772nacho@users.noreply.github.com>
2024-12-09 21:55:50 +0800

    lhance

---
+++ dist/js/gameSlider.js
@@ -0,0 +1,35 @@
+var slider = tns({
+    container: '.gameSlider__slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    mouseDrag: true,
+    edgePadding: 23,
+    controls: false,
+    controlsContainer: ".gameSlider__btn",
+    center: true,
+    nav: true,
+    navContainer: ".gameSlider__nav",
+    autoplayButtonOutput: false,
+    prevButton: ".gamePrev",
+    nextButton: ".gameNext",
+    loop: true,
+    responsive: {
+      900: {
+        controls: true,
+        edgePadding: 390,
+      }
+    }
+  });
+
+  slider.events.on('indexChanged', function () {
+    const allSlides = document.querySelectorAll('.gameSlider__items');
+    const activeIndex = slider.getInfo().index; // Get the current active index
+    allSlides.forEach((slide, index) => {
+      if (index === activeIndex) {
+        slide.classList.add('single-active');
+      } else {
+        slide.classList.remove('single-active');
+      }
+    });
+  });
